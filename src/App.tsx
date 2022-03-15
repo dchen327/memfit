25f6bcbf088bf0e637494257be16711d9e44ce54
David Chen <davidc3287@gmail.com>
2022-03-15 11:27:06 -0400

    test button

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import { Navbar } from "./components/Navbar";
 import "bulma/css/bulma.min.css";
 
 function App() {
+  const logSleep = () => {
+    const newDate = new Date();
+    console.log(newDate.toLocaleDateString());
+  };
+
   return (
     <div>
       <section className="section">
         <Navbar />
       </section>
       <section className="section">
-        <button className="button">Log Sleep</button>
+        <button className="button" onClick={logSleep}>
+          Log Sleep
+        </button>
       </section>
     </div>
   );
