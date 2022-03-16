2560381abed220e40bea4655847323bcf5f3ca72
David Chen <davidc3287@gmail.com>
2022-03-16 17:32:06 -0400

    firebase env

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import { useEffect, useState } from "react";
 import Charts from "./components/Charts";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAOP5AI_22FkCuUhsaCoCuXwp4J1CpMDQo",
-  authDomain: "mem-fit.firebaseapp.com",
-  projectId: "mem-fit",
-  storageBucket: "mem-fit.appspot.com",
-  messagingSenderId: "30218199815",
-  appId: "1:30218199815:web:0063e72bf03da378eb9ba2",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 initializeApp(firebaseConfig);
