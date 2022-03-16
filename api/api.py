4234a4f851241768a27fb5b3cf33ba44fec3cd64
David Chen <davidc3287@gmail.com>
2022-03-16 00:22:41 -0400

    firebase

--- api/api.py
+++ api/api.py
@@ -5,8 +5,22 @@ import plotly.express as px
 import pandas as pd
 import datetime
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Flask setup
 app = Flask(__name__, static_folder='../build', static_url_path='/')
 CORS(app)
+
+# Firebase setup
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+    'projectId': 'mem-fit',
+})
+
+db = firestore.client()
+sleep_col = db.collection('sleep')
 
 
 @app.route('/')
@@ -15,8 +29,8 @@ def index():
     return app.send_static_file('index.html')
 
 
-@app.route('/api/logSleep', methods=['GET', 'POST'])
+@app.route('/api/logSleep')
 @cross_origin()
 def log_sleep():
     ''' Log sleep time to firestore db '''
-    pass
+    sleep_col.add({'datetime': datetime.datetime.now()})
