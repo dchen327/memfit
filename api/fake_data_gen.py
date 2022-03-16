62c1daea8b94114eaab3e35e3718ff74ffda7a8b
David Chen <davidc3287@gmail.com>
2022-03-16 11:01:25 -0400

    sleeps

--- api/fake_data_gen.py
+++ api/fake_data_gen.py
@@ -5,6 +5,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+import random
 from faker import Faker
 from pathlib import Path
 
@@ -26,10 +27,19 @@ sleep_ref = db.collection('sleep')
 def add_sleep():
     ''' Add randomized sleep times '''
     today_date = date.today()
-    for day in range(10):
+    for day_num in range(10):
+        day = today_date - timedelta(days=day)
         # sleep between 10pm and 2am
-        earliest_sleep = datetime.combine(today_date, time(22, 0))
-        latest_sleep = datetime.combine(today_date, time(2, 0))
+        earliest_sleep = datetime.combine(day, time(22, 0))
+        latest_sleep = datetime.combine(
+            day + timedelta(day=1), time(2, 0))
+        sleep_time = fake.date_time_between(
+            earliest=earliest_sleep, latest=latest_sleep)
+        # sleep between 6-10 hours
+        wake_time = sleep_time + \
+            timedelta(minutes=random.randint(6 * 60, 10 * 60))
+        sleep_ref.add({'datetime': sleep_time})
+        sleep_ref.add({'datetime': wake_time})
 
 
 add_sleep()
