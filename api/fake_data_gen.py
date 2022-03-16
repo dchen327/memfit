70c44c99508685f82380e4b86d4935b1925f8efa
David Chen <davidc3287@gmail.com>
2022-03-16 10:44:09 -0400

    rename

--- api/fake_data_gen.py
+++ api/fake_data_gen.py
@@ -8,6 +8,8 @@ from firebase_admin import firestore
 from faker import Faker
 from pathlib import Path
 
+import datetime
+
 
 fake = Faker()
 
@@ -18,3 +20,13 @@ firebase_admin.initialize_app(cred, {'projectId': 'mem-fit'})
 db = firebase_admin.firestore.client()
 
 sleep_ref = db.collection('sleep')
+
+
+def add_sleep():
+    ''' Add randomized sleep times '''
+    today_date = datetime.date.today()
+    print(today_date)
+    # for day in range(10):
+
+
+add_sleep()
