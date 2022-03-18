0378248c2ca019ba124719960edee96d343eda9b
David Chen <davidc3287@gmail.com>
2022-03-17 20:04:32 -0400

    spacing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,9 @@
 export function Footer() {
   return (
-    <div className="column is-narrow">
+    <div
+      className="column is-narrow"
+      style={{ padding: "0rem", margin: "0rem" }}
+    >
       <footer className="footer" style={{ padding: "1rem" }}>
         <div className="content has-text-centered">
           <strong>MemFit</strong> by David Chen. Built with React, Bulma,
