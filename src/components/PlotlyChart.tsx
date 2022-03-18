4e7db106f28fb0cd5a3a91ba6bb78d3cc0bd82ce
David Chen <davidc3287@gmail.com>
2022-03-17 20:02:05 -0400

    responsive chart

--- src/components/PlotlyChart.tsx
+++ src/components/PlotlyChart.tsx
@@ -9,9 +9,17 @@ const PlotlyChart = (props: Props) => {
   // prop is a string, convert to chart object (with data and layout)
   const chart = JSON.parse(props.chartJSON);
   console.log("chart being made");
+  const layout = chart.layout;
+  layout["autosize"] = true;
+  console.log(layout);
   return (
     <>
-      <Plot data={chart.data} layout={chart.layout} />
+      <Plot
+        data={chart.data}
+        layout={layout}
+        useResizeHandler
+        style={{ width: "100%", height: "100%" }}
+      />
     </>
   );
 };
